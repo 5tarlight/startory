93e2ba78fb80d197dfc5fad872f31cdeeeff8596
5tarlight <developerstarlight@gmail.com>
2019-04-06 23:22:15 +0900

    express setting

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,17 @@
-import * as Koa from 'koa'
-import * as Router from 'koa-router'
+import * as express from 'express'
 
-const app = new Koa()
-const router = new Router()
+const app: express.Application = express()
+const router: express.Router = express.Router()
 
-router.get('/*', async (ctx) => {
-  ctx.body = 'Hello World!';
-});
+app.set('port', 80)
 
-app.use(router.routes())
+router.route('/').get((req: express.Request, res: express.Response) => {
+  console.log(req.ip + ' : portal')
+  res.end('<h1>Hello World</h1>')
+})
 
-app.listen(80)
-console.log('Server is running on port 80')
+app.use(router)
+
+app.listen(app.get('port'), () => {
+  console.log(`Express server is online on port ${app.get('port')}`)
+})
