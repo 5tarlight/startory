74a111fb225d8b0e8d23785e3d339642b6cbabcd
5tarlight <developerstarlight@gmail.com>
2019-04-08 23:22:37 +0900

    body parser ready

--- src/Server.ts
+++ src/Server.ts
@@ -1,6 +1,7 @@
 import * as express from 'express'
 import * as SLog from './SLog'
 import * as path from 'path'
+import * as bodyParser from 'body-parser'
 import Router from './routes/Router'
 
 class Server {
@@ -20,6 +21,8 @@ class Server {
     this.app.set('views', path.join(__dirname, '/public/views'))
 
     this.app.use('/', express.static(path.join(__dirname, '/public')))
+    this.app.use(bodyParser.urlencoded({ extended: false }))
+    this.app.use(bodyParser.json())
     Router.init(this.app, this.router)
   }
 
