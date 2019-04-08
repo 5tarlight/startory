4e262eacf5fd95a5c8c74f30ccf431370101084a
5tarlight <developerstarlight@gmail.com>
2019-04-08 23:20:07 +0900

    Router

--- src/routes/Router.ts
+++ src/routes/Router.ts
@@ -1,10 +1,11 @@
 import * as express from 'express'
-import { Portal } from './Routes'
+import { Portal, SignUp } from './Routes'
 import * as SLog from '../SLog'
 
 class Router {
   static init(app:express.Application, router: express.Router): void {
     router.route('/').get(Portal.join)
+    router.route('/signup').get(SignUp.join)
 
     app.use(router)
     SLog.success('Router is Ready.')
