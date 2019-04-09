35dd872bc0926340fcfcef3aa44cb9d44a313047
5tarlight <developerstarlight@gmail.com>
2019-04-09 22:14:14 +0900

    DB Manager

---
+++ src/DB.ts
@@ -0,0 +1,19 @@
+import * as mysql from 'mysql'
+import * as DBSetting from './config/dbsetting.json'
+import * as SLog from './SLog'
+
+class DB {
+  static conn: mysql.Connection
+
+  static init() {
+    this.conn = mysql.createConnection(DBSetting)
+    SLog.success('DB Connection Ready')
+  }
+
+  static query(query: string, callback: mysql.QueryFunction) {
+    this.conn.connect()
+    this.conn.query(query, callback)
+  }
+}
+
+export default DB
