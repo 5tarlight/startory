95abccd797151fe7f5cd785ee9315ba7d52c4116
5tarlight <developerstarlight@gmail.com>
2019-04-09 22:33:26 +0900

    sign up router

--- src/routes/SignUp.ts
+++ src/routes/SignUp.ts
@@ -1,5 +1,6 @@
 import * as express from 'express'
 import * as SLog from '../SLog'
+import DB from '../DB'
 
 class SignUp {
   static join(req: express.Request, res: express.Response): void {
@@ -12,6 +13,36 @@ class SignUp {
       }
 
       res.end(html)
+    })
+  }
+
+  static signup(req: express.Request, res: express.Response): void {
+    const username = req.body.username || req.query.username
+    const password = req.body.password || req.query.password
+    const exp: RegExp = /^\w\w{3,18}\w$/
+
+    if(!username || !password) {
+      res.redirect('/signup')
+      return
+    }
+
+    if(!exp.test(username) || !exp.test(password)) {
+      res.redirect('/signup')
+      return
+    }
+
+    DB.query(`INSERT INTO user (username, password) VALUES (${username}, ${password})`, (err, results, fileds) => {
+      if(err) {
+        if(err.stack) {
+          SLog.err(err.stack)
+          return
+        } else throw err
+      }
+      if(req.session) {
+        req.session.username = username
+        SLog.info(req.ip + ' : new user signed in ' + username)
+      }
+      else throw Error('Session is not defined')
     })
   }
 }
