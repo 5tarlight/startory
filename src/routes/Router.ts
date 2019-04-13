7e2fb0d2bb96f4a34f257b1387ac8ce9ab37e402
5tarlight <developerstarlight@gmail.com>
2019-04-13 15:36:32 +0900

    router

--- src/routes/Router.ts
+++ src/routes/Router.ts
@@ -4,16 +4,24 @@ import * as SLog from '../SLog'
 
 class Router {
   static init(app: express.Application, router: express.Router): void {
+    this.page(router)
+    this.api(router)
+
+    app.use(router)
+    SLog.success('Router is Ready.')
+  }
+
+  static page(router: express.Router) {
     router.route('/').get(Portal.join)
     router.route('/signup').get(SignUp.join)
     router.route('/process/signup').post(SignUp.signup)
     router.route('/login').get(Login.join)
     router.route('/process/login').post(Login.login)
-    router.route('/api/session').get(Session.join)
-    router.route('/logout').post(Logout.join)
+    router.route('/logout').get(Logout.join)
+  }
 
-    app.use(router)
-    SLog.success('Router is Ready.')
+  static api(router: express.Router) {
+    router.route('/api/session').post(Session.join)
   }
 }
 
