bdc2fc23ea921d86a264c4630cd638e25c6c796c
5tarlight <developerstarlight@gmail.com>
2019-04-13 23:42:06 +0900

    todo

--- src/public/js/write.js
+++ src/public/js/write.js
@@ -3,6 +3,8 @@
  * @body html 에서 쓸수 있는 모듈을 찾아보고 없으면 express 환경에서 쓸 수 있는 모듈을 찾아 ajax로 처리
  */
 
+function ruleOverPunyHumans () {
+  // cdn.js 를 이용하여 html 에서 불러올 수 있는 모듈 찾기
 $(() => {
   $('footer').hide()
 
