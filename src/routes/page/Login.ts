3584c3470f2c4f9cf945bbf0c45a73d5e2026e46
5tarlight <developerstarlight@gmail.com>
2019-04-13 15:36:04 +0900

    move router

---
+++ src/routes/page/Login.ts
@@ -0,0 +1,57 @@
+import * as express from 'express'
+import * as SLog from '../../SLog'
+import DB from '../../DB'
+
+class SignUp {
+  static join(req: express.Request, res: express.Response): void {
+    SLog.info(req.ip + ' : login')
+    req.app.render('login', (err: Error, html: string) => {
+      if(err) {
+        if (err.stack)
+          SLog.err(err.stack)
+        else throw err
+      }
+
+      res.end(html)
+    })
+  }
+
+  static login(req: express.Request, res: express.Response): void {
+    const username = req.body.username || req.query.username
+    const password = req.body.password || req.query.password
+    const exp: RegExp = /^\w\w{3,18}\w$/
+
+    if(!username || !password) {
+      res.redirect('/signup')
+      return
+    }
+
+    if(!exp.test(username) || !exp.test(password)) {
+      res.redirect('/signup')
+      return
+    }
+
+    DB.query(`SELECT * FROM user WHERE username="${username}" AND password="${password}"`, (err, results, fileds) => {
+      if(err) {
+        if(err.stack) {
+          SLog.err(err.stack)
+          return
+        } else throw err
+      }
+
+      if(results.length > 0) {
+        SLog.info(req.ip + ' : Logged in as ' + username)
+        if(req.session) {
+          req.session.username = username
+          res.redirect('/')
+        }
+        else throw Error('Session is not Defined')
+      } else {
+        SLog.info(req.ip + ' : failed to log in')
+        res.redirect('/login')
+      }
+    })
+  }
+}
+
+export default SignUp
---
+++ src/routes/page/SignUp.ts
@@ -0,0 +1,51 @@
+import * as express from 'express'
+import * as SLog from '../../SLog'
+import DB from '../../DB'
+
+class SignUp {
+  static join(req: express.Request, res: express.Response): void {
+    SLog.info(req.ip + ' : sign up')
+    req.app.render('signup', (err: Error, html: string) => {
+      if(err) {
+        if (err.stack)
+          SLog.err(err.stack)
+        else throw err
+      }
+
+      res.end(html)
+    })
+  }
+
+  static signup(req: express.Request, res: express.Response): void {
+    const username = req.body.username || req.query.username
+    const password = req.body.password || req.query.password
+    const exp: RegExp = /^\w\w{3,18}\w$/
+
+    if(!username || !password) {
+      res.redirect('/signup')
+      return
+    }
+
+    if(!exp.test(username) || !exp.test(password)) {
+      res.redirect('/signup')
+      return
+    }
+
+    DB.query(`INSERT INTO user (username, password) VALUES ("${username}", "${password}")`, (err, results, fileds) => {
+      if(err) {
+        if(err.stack) {
+          SLog.err(err.stack)
+          return
+        } else throw err
+      }
+      if(req.session) {
+        req.session.username = username
+        SLog.info(req.ip + ' : new user signed in ' + username)
+        res.redirect('/')
+      }
+      else throw Error('Session is not defined')
+    })
+  }
+}
+
+export default SignUp
