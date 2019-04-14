6484c934bc8e77c8d80a3b8930483434f6e960a5
5tarlight <developerstarlight@gmail.com>
2019-04-14 14:19:20 +0900

    syntax highlighting

--- src/public/js/write.js
+++ src/public/js/write.js
@@ -4,7 +4,18 @@ $(() => {
   $('.save').on('click', event => {
     event.preventDefault()
 
-    const md = $('.input-markdown').val()
-    $('.markdown').html(markdown.toHTML(md))
+    const md = window.markdownit({
+      highlight: function (str, lang) {
+        if (lang && hljs.getLanguage(lang)) {
+          try {
+            return hljs.highlight(lang, str).value;
+          } catch (__) {}
+        }
+    
+        return ''; // use external default escaping
+      }
+    })
+    const markdown = $('.input-markdown').val()
+    $('.markdown').html(md.render(markdown))
   })
 })
