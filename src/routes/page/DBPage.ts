278ff5287b469d745436c0ec6a716e9df4731db1
5tarlight <developerstarlight@gmail.com>
2019-04-21 00:24:30 +0900

    bug fix

--- src/routes/page/DBPage.ts
+++ src/routes/page/DBPage.ts
@@ -43,12 +43,12 @@ class DBPage {
     })
   }
 
-  static topic(req: express.Request, res: express.Response): void {
+  static async topic(req: express.Request, res: express.Response): Promise<void> {
     const qusername = req.params.username
     const qtopic = req.params.topic
-    let id = 0
-
+    
     DB.query('SELECT * FROM `user` WHERE `username`=?', [qusername], (err, results, fileds): void => {
+      let id = 0
       if(err) {
         SLog.err(err.stack || err.toString())
         return
@@ -56,42 +56,42 @@ class DBPage {
 
       if(results.length > 0) {
         id = results[0]['id']
-      } else {
-        req.app.render('404', {},(err: Error, html: string) => {
+
+        DB.query('SELECT * FROM topic WHERE id=? AND author=?', [parseInt(qtopic), id], (err, results, fileds): void => {
           if(err) {
             SLog.err(err.stack || err.toString())
             return
           }
-
-          res.end(html)
-          SLog.info(req.ip + ' : 404')
+          console.dir(results)
+          if(results.length > 0) {
+            res.end(`
+            <h1>${results[0]['title']}</h1>
+            <h3>Author: ${qusername}</h3>
+            <div>${results[0]['article']}
+            `)
+    
+            SLog.info(req.ip + ' : ' + qusername + '/' + qtopic)
+          } else {
+            req.app.render('404', {}, (err: Error, html: string) => {
+              if(err) {
+                SLog.err(err.stack || err.toString())
+                return
+              }
+    
+              res.end(html)
+              SLog.info(req.ip + ' : 404(topic)')
+            })
+          }
         })
-      }
-    })
-
-    DB.query('SELECT * from `topic` WHERE `author`=? AND `id`=?', [id, qtopic], (err, results, fileds): void => {
-      if(err) {
-        SLog.err(err.stack || err.toString())
-        return
-      }
-
-      if(results.length > 0) {
-        res.end(`
-        <h1>${results[0]['title']}</h1>
-        <h3>Author: ${results[0]['author']}</h3>
-        <div>${results[0]['article']}
-        `)
-
-        SLog.info(req.ip + ' : ' + qusername + '/' + qtopic)
       } else {
-        req.app.render('404', {},(err: Error, html: string) => {
+        req.app.render('404', {}, (err: Error, html: string) => {
           if(err) {
             SLog.err(err.stack || err.toString())
             return
           }
 
           res.end(html)
-          SLog.info(req.ip + ' : 404')
+          SLog.info(req.ip + ' : 404(author)')
         })
       }
     })
